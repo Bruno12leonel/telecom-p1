7f9c89e61a1e2cf3e9f49cde6f666f8d4b416873
Bruno12leonel <57578259+Bruno12leonel@users.noreply.github.com>
2022-07-05 14:09:47 -0300

    brunao

--- modem.py
+++ modem.py
@@ -1,5 +1,6 @@
 from random import sample
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Modem:
     def __init__(self, fs, bufsz, ans=False):        
@@ -15,6 +16,8 @@ class Modem:
         
         self.amostras = []
         self.phi = 0
+        self.data = []
+
         if ans:
             # inverte as frequências
             self.tx_omega0, self.rx_omega0 = self.rx_omega0, self.tx_omega0
@@ -42,7 +45,34 @@ class Modem:
 
     # Demodulação
     def put_samples(self, data):
-        return 0
+        self.data.extend(data)
+        
 
     def get_bits(self):
-        return []
+        s = self.data
+        v0i=np.zeros(len(s))
+        v0r=np.zeros(len(s))
+        v1i=np.zeros(len(s))
+        v1r=np.zeros(len(s))
+        L = self.bufsz
+        T = 1/self.fs
+
+        r=0.99
+
+        for n in range(1,len(s)):
+            v0r[n] = s[n] - r**L * np.cos(self.rx_omega0*L*T)*s[n-L] + r*np.cos(self.rx_omega0*T)*v0r[n-1] - r*np.sin(self.rx_omega0*T)*v0i[n-1]
+            v0i[n] = -r**L*np.sin(self.rx_omega0*L*T)*s[n-L] + r*np.cos(self.rx_omega0*T)*v0i[n-1] + r*np.sin(self.rx_omega0*T)*v0r[n-1]
+            v1r[n] = s[n] - r**L * np.cos(self.rx_omega1*L*T)*s[n-L] + r*np.cos(self.rx_omega1*T)*v1r[n-1] - r*np.sin(self.rx_omega1*T)*v1i[n-1]
+            v1i[n] = -r**L*np.sin(self.rx_omega1*L*T)*s[n-L] + r*np.cos(self.rx_omega1*T)*v1i[n-1] + r*np.sin(self.rx_omega1*T)*v1r[n-1]
+        
+            rho = v1r**2+v1i**2+v0r**2+v0i**2   # carrier detection
+
+            c = abs(v1r**2+v1i**2-v0r**2-v0i**2)
+            v = np.zeros(len(c))
+            y = np.zeros(len(c))
+            r=0.9999
+            for n in range(1,len(s)):
+                v[n] = (1-r)*c[n] + 2*r*np.cos(2*np.pi*300/self.fs)*v[n-1] - r**2*v[n-2]
+                y[n] = v[n] - v[n-2]
+
+        return y
