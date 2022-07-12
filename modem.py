2160eec2d04d344fa4cd1b0f72060974ddbab727
Bruno12leonel <bruno12leonel@gmail.com>
2022-07-12 17:01:19 -0300

    demo ex1

--- modem.py
+++ modem.py
@@ -19,6 +19,7 @@ class Modem:
         self.amostras = []
         self.phi = 0
         self.data = self.bufsz*[0]
+        self.bits = []
         ##
         if ans:
             # inverte as frequências
@@ -30,6 +31,7 @@ class Modem:
         for bit in bits:            
             y = np.zeros(self.bufsz)            
             t = 0.0
+            #sin(wt + phi)
             omega = (self.tx_omega0 if bit==0 else self.tx_omega1)
             self.phi = self.phi - omega*t            
             for i in range(self.bufsz):
@@ -47,19 +49,19 @@ class Modem:
 
     # Demodulação
     def put_samples(self, data):
-        self.data.extend(data)
+        self.data = data
         
 
     def get_bits(self):
         s = self.data
-        v0i=np.zeros(len(s))
-        v0r=np.zeros(len(s))
-        v1i=np.zeros(len(s))
-        v1r=np.zeros(len(s))
+        v0i = np.zeros(len(s))
+        v0r = np.zeros(len(s))
+        v1i = np.zeros(len(s))
+        v1r = np.zeros(len(s))
+        count = 46*self.fs//48000
         L = self.bufsz
         T = 1/self.fs
-
-        r=0.99
+        r = 0.99
 
         for n in range(1,len(s)):
             v0r[n] = s[n] - r**L * np.cos(self.rx_omega0*L*T)*s[n-L] + r*np.cos(self.rx_omega0*T)*v0r[n-1] - r*np.sin(self.rx_omega0*T)*v0i[n-1]
@@ -67,16 +69,25 @@ class Modem:
             v1r[n] = s[n] - r**L * np.cos(self.rx_omega1*L*T)*s[n-L] + r*np.cos(self.rx_omega1*T)*v1r[n-1] - r*np.sin(self.rx_omega1*T)*v1i[n-1]
             v1i[n] = -r**L*np.sin(self.rx_omega1*L*T)*s[n-L] + r*np.cos(self.rx_omega1*T)*v1i[n-1] + r*np.sin(self.rx_omega1*T)*v1r[n-1]            
 
-            c = abs(v1r**2+v1i**2-v0r**2-v0i**2)
-            v = np.zeros(len(c))
-            y = np.zeros(len(c))
+        c = abs(v1r**2+v1i**2-v0r**2-v0i**2)                   
+        v = np.zeros(len(c))
+        y = np.zeros(len(c))
             
-            r = 0.9999
+        for n in range(L,len(s)):
+            v[n] = (1-r)*c[n] + 2*r*np.cos(2*np.pi*300/self.fs)*v[n-1] - r**2*v[n-2]
+            y[n] = v[n] - v[n-2]
 
-            filt = signal.firwin(40, 300, pass_zero='lowpass', fs=48000)
-            for n in range(1,len(s)):
-                v[n] = (1-r)*c[n] + 2*r*np.cos(2*np.pi*300/self.fs)*v[n-1] - r**2*v[n-2]
-                y[n] = v[n] - v[n-2]
-                if y[n-1] < 0 and y[n] >= 0:
-                    contador = 46*self.fs//48000
-            return y
+        filt = signal.firwin(40, 300, pass_zero='lowpass', fs= self.fs)
+        amostra = v1r**2+v1i**2-v0r**2-v0i**2
+
+        convolvedArr = np.convolve(amostra, filt)
+        for i in range(1,len(y),1):
+            if y[i-1]<0 and y[i]>=0:
+                if len(convolvedArr) > i + count:                                        
+                    self.bits.append(1 if convolvedArr[i + count] > 0 else 0)
+                else:    
+                    self.bits.append(1 if convolvedArr[-1] > 0 else 0)
+        self.data = self.data[L:]
+    
+            
+        return self.bits
